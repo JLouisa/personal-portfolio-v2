4ebe1db2fc7cb4775b9477b92c517ec928cb290b
Jlouisa <jonathan.louisa@gmail.com>
2023-12-10 14:17:28 +0100

    Removed ReactJS and add VueJS

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// import { ref } from "vue";
+</script>
+
+<template lang="">
+  <section class="home" id="home">
+    <div class="home-content">
+      <h3>Hello I am</h3>
+      <h1>Jonathan Louisa</h1>
+      <h3>And I'm a <span class="multiple-text"></span></h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt suscipit earum ab quas vitae voluptates temporibus
+        nobis.
+      </p>
+      <div class="social-media">
+        <a href="https://www.facebook.com/jonathan.louisa" target="_blank" id=""><i class="bx bxl-meta"></i></a>
+        <a href="https://www.linkedin.com/in/jonathan-louisa" target="_blank">
+          <i class="bx bxl-linkedin-square"></i>
+        </a>
+        <a href="https://github.com/JLouisa" target="_blank"><i class="bx bxl-github"></i></a>
+      </div>
+      <div>
+        <a href="" class="btn">Download CV</a>
+      </div>
+    </div>
+    <div class="home-img">
+      <img src="./images/JAL.png" alt="my photo" />
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
